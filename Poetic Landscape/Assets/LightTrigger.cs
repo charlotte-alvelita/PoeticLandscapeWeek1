40e09c76b723e7095d9314e58638cfcf8fd02aba
Charlotte Alvelita Grillet <CharlotteGrillet@Charlotte-Grillet.local>
2015-09-03 18:01:14 -0400

    light

---
+++ Poetic Landscape/Assets/LightTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ( "This is from the start, ok?" );
+		
+		gameObject.GetComponent<Light>().enabled = true; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	//when the player walks into the rigger on this gameobject
+	void OnTriggerEnter() {
+		//get the light component on this game object, turn it off
+		gameObject.GetComponent<Light> ().enabled = false; 
+	}
+	void OnTriggerExit(){
+		//get the light component on this game object, turn it on
+		gameObject.GetComponent<Light> ().enabled = true; 
+	}
+}
